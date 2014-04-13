631d0d9040be3cca783357c78d9fa03cb069ea6a
Karen <karenpenglabs@gmail.com>
2014-04-13 15:19:58 -0400

    before rewrite server and clients

--- sio_routes.js
+++ sio_routes.js
@@ -10,6 +10,62 @@ var laptopId = [];
 var mobileId = [];
 var pinataX = Math.random() * 20 + 20;
 var pinataY = Math.random() * 20;
+var scale = 1;
+var cubeSize = 24 * scale;
+var trackSize = 12 * scale;
+
+function serverCube(a, b, id) {
+  this.a = a;
+  this.b = b;
+  this.id = id;
+  this.right = 0;
+  this.back = 0;
+  this.myLocX;
+  this.myLocY;
+  this.dis;
+}
+serverCube.prototype = {
+  update: function (lr, fb) {
+    if (lr >= -6 && lr < 6) {
+      this.right = 0;
+    } else if (lr > 6) {
+      this.right = 1;
+    } else {
+      this.right = 2;
+    }
+    if (fb >= -6 && fb < 6) {
+      this.back = 0;
+    } else if (fb > 6) {
+      this.back = 1;
+    } else {
+      this.back = 2;
+    }
+  },
+  walk: function () {
+    if (this.right === 1) {
+      this.a++;
+    } else if (this.right === 2) {
+      this.a--;
+    }
+    if (this.a < 20) this.a = 20;
+    if (this.a > 60) this.a = 60;
+
+    if (this.back === 1) {
+      this.b++;
+    } else if (this.back === 2) {
+      this.b--;
+    }
+    if (this.b < -10) this.b = -10;
+    if (this.b > 30) this.b = 30;
+    this.myLocX = this.a * trackSize;
+    this.myLocY = this.b * trackSize;
+  },
+  check: function () {
+    var gapX = this.myLocX - pinataX * trackSize;
+    var gapY = this.myLocY - pinataY * trackSize;
+    this.dis = Math.sqrt(Math.pow(gapX, 2) + Math.pow(gapY, 2));
+  }
+};
 //simple example
 module.exports = function (sio) {
   var pageOpen = 0;
